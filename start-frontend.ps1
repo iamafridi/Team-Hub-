d6cda4c164fde3b9aa39cc8b608a67af9dc6232c
iamafridi <afridiakbarifty@gmail.com>
2026-06-09 00:18:05 +0600

    chore: ignore local artifacts

--- start-frontend.ps1
+++
@@ -1,4 +0,0 @@
-$logFile = "D:\Projects\Freducloud\frontend-output.log"
-$workDir = "D:\Projects\Freducloud\frontend"
-$process = Start-Process -NoNewWindow -FilePath "npx" -ArgumentList "next dev -p 3000" -WorkingDirectory $workDir -RedirectStandardOutput $logFile -RedirectStandardError $logFile -PassThru
-Write-Host $process.Id
